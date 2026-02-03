4cb1cf0248dd59f14f82bee9b10fda71cb29f857
johanneskarrer <ge87zim@mytum.de>
2026-02-03 13:26:55 +0100

    docs: "docs": extend example usage

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,10 +1,20 @@
 //
 // Created by johan on 06.01.26.
 //
+#include <print>
+
 #include "SuffixTree.hpp"
 
 int main() {
     std::string s = "GAGCTAG$";
     suffixtrees::SuffixTree tree(s);
+
+    std::string inText = "GCT";
+    std::string notInText = "AAG";
+    bool isFound1 = tree.search(inText.begin(), inText.end());
+    bool isFound2 = tree.search(notInText.begin(), notInText.end());
+    std::println("Is string {} in text? {}", inText, isFound1);
+    std::println("Is string {} in text? {}", notInText, isFound2);
+
     return 0;
 }
