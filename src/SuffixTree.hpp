2b28dc92c817a901a07359c3293cf186e81ccb6b
johanneskarrer <ge87zim@mytum.de>
2026-02-03 13:23:10 +0100

    refactor: refactoring of Ukkonen to improve clarity

--- src/SuffixTree.hpp
+++ src/SuffixTree.hpp
@@ -53,7 +53,7 @@ class SuffixTree {
      * @tparam It Iterator type
      * @param start Iterator to walk down along
      * @param skipCount Skip skipCount chars
-     * @param start_node Node which to start descent from
+     * @param startNode Node which to start descent from
      * @return first value: pointer to closest inner node above end of tree descent\n
      *      second value: nullptr if descent ends exactly on inner node, else: pointer to closest node below end of tree descent\n
      *      third value: 0 if descent ends exactly on inner node, else: position of end of descent relative to edge label
@@ -61,20 +61,20 @@ class SuffixTree {
     template <typename It>
     std::tuple<Node *, Node *, std::size_t> skipWalkDown(It start,
         const std::size_t skipCount,
-        Node *start_node) {
-        Node *curr_node = start_node;
+        Node *startNode) {
+        Node *currNode = startNode;
 
         std::size_t advance;
         for (std::size_t i = 0; i < skipCount; i += advance) {
-            Node *child = curr_node->getChild(*start);
+            Node *child = currNode->getChild(*start);
             advance = std::min(skipCount - i, child->label.length());
             if (advance < child->label.length()) {
-                return std::make_tuple(curr_node, child, advance);
+                return std::make_tuple(currNode, child, advance);
             }
             std::advance(start, advance);
-            curr_node = child;
+            currNode = child;
         }
-        return std::make_tuple(curr_node, nullptr, 0);
+        return std::make_tuple(currNode, nullptr, 0);
     }
 
     /**
@@ -82,79 +82,71 @@ class SuffixTree {
      */
     void ukkonenConstruction() {
         assert(!str_view.empty());
-        Node *curr_node = root.get(), *child = root.get(), *suffix_link_pending = nullptr;
-        std::size_t posOnEdge = 0;
-        std::size_t jInitializer = 0;
+        Node *activeNode = root.get(), *child = root.get(), *suffixLinkPending = nullptr;
+        // skipCount represents the position of the
+        std::size_t skipCount = 0, jDash = 0;
         bool moveForward = false;
 
-        for (std::size_t i = 0; i < str_view.length(); ++i) {
+        // Iterator pointing to char which to extend with, increase in i-loop
+        auto prefixEndIt = str_view.begin();
+        for (std::size_t i = 0; i < str_view.length(); ++i, ++prefixEndIt) {
             // Phase i
-            auto prefix_end_it = str_view.begin() + i;
+            for (std::size_t j = jDash; j <= i; ++j) {
+                // Phase j
 
-            for (std::size_t j = jInitializer; j <= i; ++j) {
-                std::string_view &gamma = child->label;
-
-                auto start = gamma.begin();
-                std::size_t skipCount = posOnEdge;
-                Node *startNode = curr_node;
-
+                // Start skipping characters at suffix link if present or in root
+                // Iterator to sequence we went up along to closest inner node
+                auto skipLabelIt = child->label.begin();
+                Node *startNode = activeNode;
                 if (moveForward) {
-                    startNode = curr_node->suffixLink;
-                    if (curr_node == root.get()) {
-                        if (skipCount > 0) --skipCount; // TODO this should never be triggered if skipCount is 0; test that it actually doesn't (except in first iteration?)
-                        ++start;
+                    startNode = activeNode->suffixLink;
+                    if (activeNode == root.get()) {
+                        skipCount = skipCount > 0 ? --skipCount : 0;
+                        ++skipLabelIt;
                     }
                 }
 
-                auto walkDownInformation = skipWalkDown(start, skipCount, startNode); // basically step 2 of SEA
+                std::tie(activeNode, child, skipCount) = skipWalkDown(skipLabelIt, skipCount, startNode);
+                child = !child ? activeNode->getChild(*prefixEndIt) : child;
 
-                std::tie(curr_node, child, posOnEdge) = walkDownInformation;
-
-                if (!child) {
-                    if (suffix_link_pending) suffix_link_pending->suffixLink = curr_node;
-
-                    if (curr_node->getChild(*prefix_end_it)) {
-                        // Rule 3: String implicitly in tree, break
-                        ++posOnEdge;
-                        child = curr_node->getChild(*prefix_end_it);
-                        moveForward = false;
-                        suffix_link_pending = nullptr;
-                        break;
-                    } else {
-                        // Rule 2: Split by creating new child node
-                        ++jInitializer;
-                        moveForward = true;
-                        child = &curr_node->constructChild(*prefix_end_it,
-                            std::string_view(prefix_end_it, str_view.end()), j);
-                        suffix_link_pending = nullptr;
-                    }
+                if (child && child->label[skipCount] == *prefixEndIt) {
+                    // Rule 3: String implicitly in tree, break
+                    if (suffixLinkPending) suffixLinkPending->suffixLink = activeNode;
+                    suffixLinkPending = nullptr;
+                    // Do not increase j
+                    moveForward = false;
+                    // Skip one more char in next iteration (i is increased)
+                    skipCount = (skipCount + 1) % child->label.length();
+                    if (skipCount == 0) activeNode = child;
+                    break;
                 } else {
-                    if (child->label[posOnEdge] == *prefix_end_it) {
-                        // Rule 3: String implicitly in tree, break
-                        posOnEdge = (posOnEdge + 1) % child->label.length(); // TODO update curr_node???
-                        if (posOnEdge == 0) {
-                            curr_node = child;
-                        }
-                        moveForward = false;
-                        suffix_link_pending = nullptr;
-                        break;
-                    }
-                    // Rule 2: Split by splitting an edge
-                    ++jInitializer;
+                    // Rule 2
+                    ++jDash;
                     moveForward = true;
-                    char oldChildIndex = *child->label.begin();
-                    std::unique_ptr<NodeT> oldChild = curr_node->extractChild(oldChildIndex);
-                    // Create new node at split and new leaf as its child
-                    Node &newNode = curr_node->
-                        constructChild(oldChildIndex, oldChild->label.substr(0, posOnEdge));
-                    newNode.constructChild(*prefix_end_it, std::string_view(prefix_end_it, str_view.end()), j);
-                    oldChild->trimLabeltoSuffix(posOnEdge);
+                    if (!child) {
+                        // Rule 2 case 1: Split by creating new child node
+                        if (suffixLinkPending) suffixLinkPending->suffixLink = activeNode;
+                        child = &activeNode->constructChild(*prefixEndIt,
+                            std::string_view(prefixEndIt, str_view.end()), j);
+                        suffixLinkPending = nullptr;
+                        continue;
+                    }
 
-                    // Move old child to child of newly created node
+                    // Rule 2 case 2: Split by splitting an edge
+                    char oldChildIndex = *child->label.begin();
+                    std::unique_ptr<NodeT> oldChild = activeNode->extractChild(oldChildIndex);
+                    // Create new node at split, new leaf and move oldChild
+                    Node &newNode = activeNode->
+                        constructChild(oldChildIndex, oldChild->label.substr(0, skipCount));
+                    newNode.constructChild(*prefixEndIt, std::string_view(prefixEndIt, str_view.end()), j);
+                    oldChild->trimLabeltoSuffix(skipCount);
                     newNode.insertChild(*oldChild->label.begin(), std::move(oldChild));
-                    if (suffix_link_pending) suffix_link_pending->suffixLink = &newNode;
-                    // Will get its suffix link in phase j+1
-                    suffix_link_pending = &newNode;
+
+                    // Set suffix link and mark newNode as in need of suffix link
+                    if (suffixLinkPending) suffixLinkPending->suffixLink = &newNode;
+                    suffixLinkPending = &newNode;
+                    // Update child from oldChild to newNode
+                    // Leave curr_node as is to use its suffix link in next iteration.
                     child = &newNode;
                 }
             }
